733ae6d0059d76716109f940c015f08405cff452
Brynfor Morris <brynfor.morris@gmail.com>
2023-02-22 16:57:30 -0600

    passed props down

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,16 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const [home,about,projects] = links;
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+  <nav>{
+    <div>
+  <a href={"#"+ home} key = '1'>{home}</a>
+  <a href={"#"+ about} key = '2'>{about}</a>
+  <a href={"#"+ projects} key = '3'>{projects}</a>
+  </div>
+  }</nav>);
 }
 
 export default NavBar;
